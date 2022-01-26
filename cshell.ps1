02aabaeea1b96602845a71f428cfa14d09b55e26
Tyler887 <tylermageeshields@gmail.com>
2022-01-26 17:57:09 +0000

    Update cshell.ps1

--- cshell.ps1
+++ cshell.ps1
@@ -2,12 +2,13 @@ $v = 1.0
 echo "ConShell $v"
 $games = cd
 if (-not Test-Path $games/games) {
-  echo "Could not find Games directory."
+  echo "Welcome to CShell! Just setting up your space..."
+  Add-Item "$games/games" -ItemType Directory
 }
 cd games
 echo "GAMES:"
 Get-ChildItem -Path "$games/cshell" -Filter *.psgame -r | % { $_.Name.Replace( ".ps1","") }
 $name = Read-Host 'Enter the name of the game to create. You can edit it later in your local notepad.'
 Add-Item "$name.ps1"
-echo "# Welcome! ConShell is an open source game engine`n# for PowerShell.`n# Here is a simple game for you.`necho Hello World"  >>
+echo "# Welcome! ConShell is an open source game engine`n# for PowerShell.`n# Here is a simple game for you.`necho Hello World" >> "$name.ps1"
 notepad $name.ps1
