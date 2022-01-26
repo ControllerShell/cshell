89df6a78d872754dab20b4a05dbe0684c3977a2c
Tyler887 <tylermageeshields@gmail.com>
2022-01-26 17:59:52 +0000

    Update and rename cshell.ps1 to cshell-devs.ps1

--- cshell-devs.ps1
+++ cshell-devs.ps1
@@ -10,5 +10,8 @@ echo "GAMES:"
 Get-ChildItem -Path "$games/cshell" -Filter *.psgame -r | % { $_.Name.Replace( ".ps1","") }
 $name = Read-Host 'Enter the name of the game to create. You can edit it later in your local notepad.'
 Add-Item "$name.ps1"
-echo "# Welcome! ConShell is an open source game engine`n# for PowerShell.`n# Here is a simple game for you.`necho Hello World" >> "$name.ps1"
+echo "# Welcome! ConShell is an open source game engine`n# for PowerShell.`n# Here is a simple game for you.`necho Hello World!`necho Use ConShell to develop`necho simple powershell games." >> "$name.ps1"
 notepad $name.ps1
+echo "Game Created. Exiting in 5 seconds."
+Start-Sleep -s 5
+exit
