6d55d15c111897cd8b598115bcc962a5c30f453a
Tyler887 <tylermageeshields@gmail.com>
2022-01-26 17:48:57 +0000

    Update cshell.ps1

--- cshell.ps1
+++ cshell.ps1
@@ -2,4 +2,8 @@ $v = 1.0
 echo "ConShell $v"
 echo "processing"
 $games = cd
-Get-ChildItem -Path $games"/cshell" -Filter *.psgame -r | % { $_.Name.Replace( ".psgame"," game") }
+
+echo "GAMES:"
+Get-ChildItem -Path $games"$games/cshell" -Filter *.psgame -r | % { $_.Name.Replace( ".psgame"," game") }
+$name = Read-Host 'Enter the name of the game to create. You can edit it later in your local notepad.'
+notepad $name.ps1
