6caa2054ab995479134dd0e020ae5cc0f9d6680f
Tyler887 <tylermageeshields@gmail.com>
2022-01-29 18:02:18 +0000

    Update cshell-servers.ps1

--- cshell-servers.ps1
+++ cshell-servers.ps1
@@ -3,7 +3,7 @@ echo "Generating ID of game"
 for ($i = 1; $i -le 30; $i++ ) {
   Write-Host "." -NoNewline
   $seed = Get-Random -Minimum 300
-  $r = Get-Random -Minimum 3 -Maximum 10 -Seed $seed
+  $r = Get-Random -Minimum 3 -Maximum 10 -SetSeed $seed
   $r2 = Get-Random -Minimum 50 -Maximum 125
   $global:Id = "$global:Id$r"
   Start-Sleep -ms $r2
