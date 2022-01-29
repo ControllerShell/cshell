15c1dca6291b53127d6e2f6079974aa64dfa9870
Tyler887 <tylermageeshields@gmail.com>
2022-01-29 17:51:08 +0000

    Update cshell-servers.ps1

--- cshell-servers.ps1
+++ cshell-servers.ps1
@@ -4,7 +4,7 @@ for ($i = 1; $i -le 30; $i++ ) {
   Write-Host "." -NoNewline
   $r = Get-Random -Minimum 3 -Maximum 10
   $r2 = Get-Random -Minimum 50 -Maximum 125
-  echo "$r" -NoNewline >> $global:Id
+  $global:Id = "$global:Id$r"
   Start-Sleep -ms $r2
 }
 echo "Game ID: $global:Id"
