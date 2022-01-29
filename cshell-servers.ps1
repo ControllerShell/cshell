ed5fb49bd730a43a53f44546975f72f2b86d4163
Tyler887 <tylermageeshields@gmail.com>
2022-01-29 19:34:39 +0000

    Update cshell-servers.ps1

--- cshell-servers.ps1
+++ cshell-servers.ps1
@@ -2,11 +2,12 @@ echo "ConShell Server Edition"
 echo "Generating ID of game"
 for ($i = 1; $i -le 30; $i++ ) {
   Write-Host "." -NoNewline
-  $seed = Get-Random -Minimum 300
+  $seed = Get-Random -Minimum 3000
   $r = Get-Random -Minimum 3 -Maximum 10 -SetSeed $seed
-  $r2 = Get-Random -Minimum 50 -Maximum 125
+  $r2 = Get-Random -Minimum 50 -Maximum 180
   $global:Id = "$global:Id$r"
   Start-Sleep -ms $r2
 }
 echo "Game ID: $global:Id"
 echo "# Welcome to your new game.`n# Write some code below" >> "$global:Id.ps1"
+n
